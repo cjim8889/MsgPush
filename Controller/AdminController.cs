586aebede9fa6eeb8164c089e2f80a0b12e5dc49
Wuhao Chen <c.jim8889@outlook.com>
2019-05-09 23:14:44 +0000

    test my CI/CD

--- Controller/AdminController.cs
+++ Controller/AdminController.cs
@@ -86,6 +86,12 @@ namespace MsgPush.Controller
             return CreatedAtAction("CreateUser", user);
         }
 
+        [HttpGet("testCd")]
+        public ActionResult TestCdPipeline()
+        {
+            return Ok(new {StatusCode="Very Good"});
+        }
+
 
     }
 }
