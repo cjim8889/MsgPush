c04006f65791632e2f24fde56af3fd27fbd2dc68
Wuhao Chen <43540548+cjim8889@users.noreply.github.com>
2019-05-09 12:16:40 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export Jwt_SecretKey="aiosdjfoj203984520g3i4;asldf[pasdf.a;s,dflp[qlw[cl1234]]]"
-export Recaptcha_SecretKey="6LcW5KEUAAAAAJUlB1r539Z98-A5f-sNZbrLgE5t"
-export SecretKey=IAOSJDFOAJS91023junaidaisdf
-
-dotnet run
